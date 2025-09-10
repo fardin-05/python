3dd2e4a42e5be5bd8b0f3930001d6911bb614c56
Fardin Khan <azimahmed9722@gmail.com>
2025-09-10 14:15:54 +0600

    Update sum.py

--- sum.py
+++ sum.py
@@ -1,3 +1,11 @@
+#sum in every each number
 My_List = [1,2,3,4,5]
-sum= sum(My_List)
-print('All Addition is :',sum )
+total = 0
+for i in My_List :
+    total=total+i 
+    print("Answer:",total )
+
+#sum in all list numbers 
+My_List_2 = My_List
+total= sum(My_List)
+print("Total:",total)
