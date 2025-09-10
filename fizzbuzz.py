6b99c1d4ff11b4c3d150a3aa980f315aefeec26c
Fardin Khan <azimahmed9722@gmail.com>
2025-09-10 17:54:21 +0600

    Add files via upload

---
+++ fizzbuzz.py
@@ -0,0 +1,12 @@
+# fizzbuzz
+a=int(input("Enter Your Numebr : "))
+if a % 3 == 0 and a % 5 == 0 :
+    print("fizzbuzz")
+elif a % 3 == 0 :
+    print ('fizz')
+elif a % 5 == 0 :
+
+    print ('buzz')
+else:
+    print('null')   
+    
