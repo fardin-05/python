2f8735bc239560c824cd508507d347ba90f6cbb6
Fardin Khan <azimahmed9722@gmail.com>
2025-09-10 17:24:42 +0600

    Add files via upload

---
+++ dict.py
@@ -0,0 +1,11 @@
+my_dict={"a" : 10 , "b" : "fardin" , "c" : True}
+my_dict_2={"d" : 25 , "e" : "Khan" , "f" : True}
+merge_dict= my_dict | my_dict_2 # use pipe operator to merge my_dict and my_dict_2 
+#if change the value my_dict
+'''
+merge_dict['a' ]=20 
+merge_dict['e']='Azim'
+
+'''
+
+print(merge_dict)
